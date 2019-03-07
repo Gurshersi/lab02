65e265993c43015c7d06c644def68546cc28f99a
visualstudio <visualstudio@230-PC10>
2019-03-07 16:06:04 -0500

    lab02 c0715837

--- lab02/Program.cs
+++ lab02/Program.cs
@@ -10,6 +10,40 @@ namespace lab02
     {
         static void Main(string[] args)
         {
+            //Student name Gursher Singh Student ID c0715837
+            //CSD3354 Section2
+            //Assignment 2
+            //March 7,2019
+        }
+    }
+}
+
+public class DelegateExercises
+{
+    public delegate void MyDelegate();
+
+    void Method1()
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+
+    }
+
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+
+    }
+}
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TO DO: Call Method2 from the DelegateExercises Class
         }
     }
 }
